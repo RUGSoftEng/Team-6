a99f699f5f764b6a6270a4494123374bb8513127
Thijs <knaap@live.com>
2016-03-22 14:19:51 +0100

    added chromecast support, but interface still has to be perfected

---
+++ Quiz/Assets/Scripts/Quiz/CastCameraSwitch.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Google.Cast.RemoteDisplay;
+using System.Collections;
+
+public class CastCameraSwitch : MonoBehaviour {
+
+    public CastRemoteDisplayManager chromeCast;
+    public Camera mainCamera;
+    public Camera phoneCamera;
+
+    public void Start()
+    {
+        chromeCast.RemoteDisplaySessionStartEvent.AddListener(startCasting);
+        chromeCast.RemoteDisplaySessionEndEvent.AddListener(stopCasting);
+        chromeCast.RemoteDisplayErrorEvent.AddListener(errorCasting);
+        phoneCamera.enabled = false;
+    }
+
+    public void startCasting(CastRemoteDisplayManager c)
+    {
+        chromeCast.RemoteDisplayCamera = mainCamera;
+        phoneCamera.enabled = true;
+    }
+
+    public void stopCasting(CastRemoteDisplayManager c)
+    {
+        chromeCast.RemoteDisplayCamera = null;
+        phoneCamera.enabled = false;
+        mainCamera.enabled = true;
+    }
+
+    public void errorCasting(CastRemoteDisplayManager c)
+    {
+        phoneCamera.enabled = false;
+        mainCamera.enabled = true;
+    }
+}
