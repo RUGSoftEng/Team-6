e3d526676920db074f9af4cd0bd624501b1f862b
thijs knaap <thijs knaap>
2016-04-18 16:29:54 +0200

    added a Timer, is not yet used since we have to discuss what we want to time

---
+++ Quiz/Assets/Scripts/Quiz/Timer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+    double currentTime;
+    bool timing = false;
+
+	// Starts the timing, when a timing is allready running nothing is done.
+	void StartTiming () {
+	    if (!timing)
+        {
+            currentTime = 0;
+        }
+	}
+	
+	// Update is called once per frame, it updates the time that is timed untill now.
+	void Update () {
+        if (timing)
+        {
+            currentTime = currentTime + Time.deltaTime;
+        }
+	}
+
+    //Stops the timer and returns the amount of time that is timed.
+    double StopTiming()
+    {
+        timing = false;
+        return currentTime;
+    }
+}
