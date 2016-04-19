8682968b4bda8757e56e75339113c6998b07c730
thijs knaap <thijs knaap>
2016-04-19 15:11:53 +0200

    generalizing game

---
+++ Quiz/Assets/Scripts/GeneralGame/AbstractController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AbstractController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    /* this method should always be called if the quiz game is exitted */
+    protected void Exit()
+    {
+        Screen.orientation = ScreenOrientation.Portrait;
+        this.GetComponent<LoadNewLevel>().LoadLevel();
+    }
+}
