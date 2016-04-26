d07042575a27291736368854f43fd6c57404e5aa
thijs knaap <thijs knaap>
2016-04-26 14:26:35 +0200

    Added back button for android

--- Quiz/Assets/Scripts/GeneralGame/AbstractController.cs
+++ Quiz/Assets/Scripts/GeneralGame/AbstractController.cs
@@ -6,8 +6,8 @@ public class AbstractController : MonoBehaviour {
 
     public List<WordData> totalWordList;
 
-    /* this method should always be called if the quiz game is exitted */
-    protected void Exit()
+    /* this method should always be called if a game is quit */
+    public void Exit()
     {
         Screen.orientation = ScreenOrientation.Portrait;
         this.GetComponent<LoadNewLevel>().LoadLevel();
