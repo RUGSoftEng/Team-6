05fdb9686f53af04da13f0c6bea74581e079cbfc
Mathijs de Jager <mathijs@inbox.lv>
2016-05-10 14:45:18 +0200

    LoadLevel text (for Jannick)

--- Quiz/Assets/Scripts/MainMenu/LoadNewLevel.cs
+++ Quiz/Assets/Scripts/MainMenu/LoadNewLevel.cs
@@ -4,14 +4,39 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class LoadNewLevel : MonoBehaviour {
 	public int minWords;
+	public Text text;
 	
 	public void LoadLevel(int index) {
 		GameObject zd = GameObject.FindWithTag("ZeeguuData");
-        if (zd.GetComponent<ZeeguuData>().userBookmarks.Count>minWords) {
+        if (false/*zd==null || zd.GetComponent<ZeeguuData>().userBookmarks.Count>minWords*/) {
 			Application.LoadLevel(index);
+		} else {
+			Color c = text.color;
+			c.a = 10;
+			text.color = c;
+			text.GetComponent<Text>().text = "You need at least "+minWords+" words to play that!";
+			StartCoroutine(FadeText());
+		}
+    }
+	
+	public IEnumerator FadeText()
+    {
+		Color c = text.color;
+		yield return new WaitForSeconds(1F);
+		c.a = 0.99F;
+		text.color = c;
+		for(int i=0;i<=25;i++) {
+			yield return new WaitForSeconds(0.04F);
+			c = text.color;
+			if (c.a>1) {
+				return true;
+			}
+			c.a -= 0.04F;
+			text.color = c;
 		}
     }
 }
