11b93a60c91674196204dcd4fe0440c7150d5b3d
Jannick <Jannickboonstra@live.nl>
2016-05-10 11:33:32 +0200

    logging in with another account refreshes bookmarks

--- Quiz/Assets/Logout.cs
+++ Quiz/Assets/Logout.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using System.IO;
 
 public class Logout : MonoBehaviour {
 
 	public void SignOut() {
+		if(File.Exists(Application.persistentDataPath + "bookmarks")) {
+			File.Delete(Application.persistentDataPath + "bookmarks");
+		}
+		
         foreach (GameObject obj in GameObject.FindGameObjectsWithTag("ZeeguuData")){
             Destroy(obj);
         }
