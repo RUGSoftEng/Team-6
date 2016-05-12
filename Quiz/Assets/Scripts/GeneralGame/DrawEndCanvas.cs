16ba130166c08c45160f7e5b73b0e7e6059a910d
thijs knaap <thijs knaap>
2016-05-12 22:59:56 +0200

    added prefab of finished screen, and added image for the background

---
+++ Quiz/Assets/Scripts/GeneralGame/DrawEndCanvas.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DrawEndCanvas : MonoBehaviour {
+    public Camera phoneCamera;
+    public Camera chromeCastCamera;
+    public Canvas phoneCanvas;
+    public Canvas chromeCanvas;
+    public Canvas endCanvas;
+
+    public void Start()
+    {
+        endCanvas.enabled = false;
+    }
+
+    public void EndScreen()
+    {
+        Canvas phone = Instantiate(endCanvas);
+        Canvas cast = Instantiate(endCanvas);
+
+        phone.enabled = true;
+        phoneCanvas.enabled = false;
+        phone.worldCamera = phoneCamera;
+
+        cast.enabled = true;
+        chromeCanvas.enabled = false;
+        cast.worldCamera = chromeCastCamera;
+        GraphicRaycaster gR = cast.GetComponent<GraphicRaycaster>();
+        gR.enabled = false;
+    }
+}
