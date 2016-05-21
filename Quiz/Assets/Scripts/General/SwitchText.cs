5eb967a1d93451e503b8d547e8beaa6cd8a84560
thijs knaap <thijs knaap>
2016-05-21 14:03:52 +0200

    Now the manatee talks!!

---
+++ Quiz/Assets/Scripts/General/SwitchText.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class SwitchText : MonoBehaviour {
+
+    public List<string> strList;
+    
+	void Start () {
+        this.GetComponent<Text>().text = strList[Random.Range(0, strList.Count)]; ;
+	}
+
+}
