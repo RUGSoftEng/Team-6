6ce6c81c56ed748456ea7a53679752a96c897516
Yannick Stoffers <y.stoffers@me.com>
2016-05-31 14:06:39 +0200

    Changed the logout and refresh bookmarks buttons. Replace the image placeholder and voila.

---
+++ Quiz/Assets/Images/NewScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		RectTransform rt = GetComponent<RectTransform> ();
+        rt.anchorMin = new Vector2 (0.5F, 0.5F);
+        rt.anchorMax = new Vector2 (0.5F, 0.5F); 
+        rt.offsetMin = new Vector2 (-90, -76);
+        rt.offsetMax = new Vector2 (90, 76);
+        rt.localScale = new Vector3 (1, 1, 1);
+        rt.localPosition = new Vector3 (0, 40, 0);
+	}
+	
+	// Update is called once per frame
+	void Update 	() {
+		transform.Rotate (Vector3.forward * 5);
+	}
+}
