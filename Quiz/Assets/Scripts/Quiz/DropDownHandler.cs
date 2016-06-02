5e4b50c86a30843577900c3d7f19ad49509653af
thijs knaap <thijs knaap>
2016-06-02 23:11:23 +0200

    Dropdown menu on new location

--- Quiz/Assets/Scripts/Quiz/DropDownHandler.cs
+++ Quiz/Assets/Scripts/Quiz/DropDownHandler.cs
@@ -7,20 +7,25 @@ public class DropDownHandler : MonoBehaviour {
     public List<string> strList;
     public Button selectable;
     public Canvas onCanvas;
+    public GameObject mainButton;
     private bool open = false, mayBeClosed = false;
     private List<Button> optionList = new List<Button>();
     // Use this for initialization
 
     void Start()
     {
+        if (strList.Count < 1)
+        {
+            strList = mainButton.GetComponent<DropDownHandler>().GetStrList();
+        }
         for (int i = 0; i < strList.Count; i++)
         {
             Button option = Instantiate(selectable) as Button;
             option.transform.SetParent(onCanvas.transform);
             option.transform.localScale = new Vector3(1, 1, 1);
             RectTransform rt = option.GetComponent<RectTransform>();
-            rt.anchorMin = new Vector2(0.08f, 1 - 0.15f * (i + 1));
-            rt.anchorMax = new Vector2(0.4f, 1 - 0.15f * (i));
+            rt.anchorMin = new Vector2(0.56f, 1 - 0.15f * (i + 1));
+            rt.anchorMax = new Vector2(0.88f, 1 - 0.15f * (i));
             rt.offsetMin = new Vector2(0, 0);
             rt.offsetMax = new Vector2(0, 0);
             rt.GetChild(0).GetComponent<Text>().text = strList[i]; ;
@@ -66,5 +71,10 @@ public class DropDownHandler : MonoBehaviour {
             option.GetComponent<RectTransform>().SetAsFirstSibling();
             option.GetComponent<RectTransform>().GetChild(0).GetComponent<Text>().color = Color.clear;
         }
+    }
+
+    public List<string> GetStrList()
+    {
+        return strList;
     }
 }
